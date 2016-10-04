7c369f8ac8682c4b18b2a98d2743a693450deb1d
Cerberus911 <tushar.krishnan92@gmail.com>
2016-10-04 19:38:03 -0400

    Add files via upload

---
+++ a4/Assignment 4.ino
@@ -0,0 +1,38 @@
+const int buttonPin = 3;    // pin to read button
+const int oddLED = 9;       // LED lit on odd button presses
+const int evenLED = 10;     // LED lit on even button presses
+int buttonState =0;
+int pressCnt = 0;           // total # of button presses
+
+void setup()
+{
+
+  pinMode(buttonPin, INPUT);
+
+}
+
+void loop()
+{
+  // if button is pressed
+  if( digitalRead(buttonPin) ) {
+    
+    // increment count
+    pressCnt++;
+    
+    // turn on even or odd LED
+    digitalWrite(oddLED, pressCnt % 2);
+    digitalWrite(evenLED, !(pressCnt % 2));
+    
+    // debounce
+    delay(1000);
+  }
+
+if (buttonState == HIGH)
+
+{
+digitalWrite(oddLED, LOW);
+    digitalWrite(evenLED, LOW);
+}
+
+}
+
