79e08069d0b4b4e8bb210430da682601f57a9a0c
Cerberus911 <tushar.krishnan92@gmail.com>
2016-10-10 14:47:03 -0400

    Add files via upload

---
+++ Assignment 5/analog_joystick_assignment-5.ino
@@ -0,0 +1,31 @@
+int xVal; //X values from joystick
+
+int yVal; //Y values from joystick
+
+void setup() {
+
+Serial.begin(9600); //Starts serial at 9600 baud
+
+pinMode(A0, INPUT); //Sets the analog ports used to an input
+
+pinMode(A2, INPUT);
+
+}
+
+void loop() {
+
+xVal = analogRead(A0); //sets the X value
+
+yVal = analogRead(A2); //sets the Y value
+
+Serial.print(" Y is...");
+
+Serial.print(yVal); //prints Y values
+
+Serial.print(" X is...");
+
+Serial.print(xVal);
+
+delay(1000); 
+}
+ //prints X values
---
+++ Assignment 5/readme.md
@@ -0,0 +1,4 @@
+# **CC Lab Assignment 5** #
+
+I have used an analog joystick sensor for this assignment. After hooking the sensor to the arduino via the breadboard, I wrote code to monitor x and y axes of the joystick in real-time on the console window.
+
